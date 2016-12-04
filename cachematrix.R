ee81febe86964c0749bae6cf5725eca309e4215e
PatriciaTorresC <torrespatricia2c@gmail.com>
2016-12-04 00:39:43 -0500

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,17 +2,17 @@
 #Below is a function that are used to create a special object that 
 #stores a matrix and caches its inverse.
 
-## This function creates a special "matrix" object that can cache its inverse.
+## makeCacheMatrix is a function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-  Inv <- NULL
-  set <- function(y) {
+  Inv <- NULL                                           #assigning the initial value
+  set <- function(y) {                                  #setting the matrix
     x <<- y
     Inv <<- NULL
   }
-  get <- function() x
-  setInverse <- function(inverse) Inv <<- inverse
-  getInverse <- function() Inv
+  get <- function() x                                   #getting the matrix
+  setInverse <- function(inverse) Inv <<- inverse       #setting the inverse
+  getInverse <- function() Inv                          #getting the matrix
   list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
@@ -28,9 +28,17 @@ cacheSolve <- function(x, ...) {
     message("getting cached data")
     return(Inv)
   }
+  ## Test if the matrix has inverse
   mat <- x$get()
-  Inv <- solve(mat, ...)
+  if(det(mat)==0){
+    return("The matrix doesn't have determinant. Please change the input.")
+  }
+  Inv <- solve(mat, ...)                                #getting the inverse
   x$setInverse(Inv)
   Inv
 }
 
+## Check the function
+x <- matrix(rnorm(9,25,50),3,3)
+y <- makeCacheMatrix(x)
+cacheSolve(y)
